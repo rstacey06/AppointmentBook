dbcbd169b2ed2e1d998cf11f1a07559755c96d20
github username <github email address>
2020-02-20 11:19:31 -0500

    first commmit

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
+
+  has_many :clients
+  has_many :appointments
+  has_many :locations
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+
+  def upcoming_appointments
+    appointments.order(appointment_time: :desc).select { |a| a.appointment_time > (DateTime.now) }
+  end
+
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,1 @@
+Rails.application.config.session_store :cookie_store, key: '_dlm_appointments_session'
